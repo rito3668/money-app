e367da9cdc01ea446d0e34d0e381ff490f18ffeb
Ritabrata Pal <rito3668@gmail.com>
2023-11-11 13:36:45 +0530

    signup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,29 @@
 import { useState } from "react"
 import { projectAuth } from "../firebase/config"
 export const useSignup =()=>{
-    
+    const [error,setError] =useState(null)
+    const [pending,setPending]=useState(false)
+    const signup = async(email,password,displayName)=>{
+        setError(null)
+        setPending(true)
+        try{
+            //signing up the user
+           const res = await projectAuth.createUserWithEmailAndPassword(email,password)
+            console.log(res.user)
+            if(!res){
+                throw new Error('Could not complete signup')
+            }
+            //add display name to user 
+            await res.user.updateProfile({displayName})
+            setPending(false)
+            setError(null)
+        }catch(err){
+            console.log(err.message)
+            setError(err.message)
+            setPending(false)
+        }
+    }
+
+
+    return {error,pending,signup}
 }
