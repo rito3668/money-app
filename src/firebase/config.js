2f53ac96f20c41808752c576063a6b91fe34c1df
Ritabrata Pal <rito3668@gmail.com>
2023-11-11 13:09:34 +0530

    importing authentication

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
+import 'firebase/auth'
 const firebaseConfig = {
     apiKey: "AIzaSyDxrAMQ1ltZE44knL3UT7kjpAwEgoeFpFU",
     authDomain: "mymoney-3e6b8.firebaseapp.com",
@@ -8,4 +9,8 @@ const firebaseConfig = {
     messagingSenderId: "968944297376",
     appId: "1:968944297376:web:8de5c42d3abb42c25c30c5"
   };
-//init   
+//init app
+firebase.initializeApp(firebaseConfig)
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+export {projectFirestore}
