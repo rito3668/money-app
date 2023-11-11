3975a25109c9c3182c1bea2518443edf587c5066
Ritabrata Pal <rito3668@gmail.com>
2023-11-11 12:57:34 +0530

    installing firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyDxrAMQ1ltZE44knL3UT7kjpAwEgoeFpFU",
+    authDomain: "mymoney-3e6b8.firebaseapp.com",
+    projectId: "mymoney-3e6b8",
+    storageBucket: "mymoney-3e6b8.appspot.com",
+    messagingSenderId: "968944297376",
+    appId: "1:968944297376:web:8de5c42d3abb42c25c30c5"
+  };
+//init   
