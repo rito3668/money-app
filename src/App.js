c30f5354e6cee0d0c3ab701d2ee14db595a9fe81
Ritabrata Pal <rito3668@gmail.com>
2023-11-11 10:19:25 +0530

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter ,Route,Switch} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+
 function App() {
   return (
     <div className="App">
       
+
+
+      <BrowserRouter>
+      
+          <Switch>
+            <Route exact path='/' >
+                <Home/>
+            </Route>
+            <Route path='/signup'>
+              <Signup/>
+            </Route>
+            <Route path='/login'>
+              <Login/>
+            </Route>
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import styles from './Navbar.module.css'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
