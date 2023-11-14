ec76949e5cd175fff4d8c135437c5fd5bb285217
Ritabrata Pal <rito3668@gmail.com>
2023-11-14 22:12:46 +0530

    transaction form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,13 @@ import React from 'react'
 import styles from './Home.module.css'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        Transaction List
+      </div>
+      <div className={styles.sidebar}>
+        form
+      </div>
+    </div>
   )
 }
---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+export default function TransactionForm() {
+    const [name,setName] = useState('')
+    const [amount,setAmount] = useState('')
+    return (
+    <>
+        <h3>Add a Transaction</h3>
+        <form>
+            <label>
+                <span>Transaction Name:</span>
+                <input 
+                    type="text"
+                    required
+                    onChange={(e)=>setName(e.target.value)}
+                    value={name}
+                />    
+            </label>   
+            <label>
+                <span>Amount($):</span>
+                <input 
+                    type="number"
+                    required
+                    onChange={(e)=>setAmount(e.target.value)}
+                    value={amount}
+                />    
+            </label>    
+        </form>  
+    </>
+  )
+}
