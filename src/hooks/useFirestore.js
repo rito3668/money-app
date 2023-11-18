ca61f79288cbccb25fc14759333594352b62cff8
Ritabrata Pal <rito3668@gmail.com>
2023-11-18 09:33:23 +0530

    firestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -9,6 +9,12 @@ let initialState = {
 }
 const firestoreReducer =(state,action)=>{
     switch(action.type){
+        case 'IS_PENDING':
+            return {pending:true,error:null,success:false,document:null}
+        case 'ADDED_DOCUMENT':
+            return {document:action.payload,pending:false,success:true,error:null}
+        case 'ERROR':
+            return {error:action.payload,pending:false,success:false,document:null}    
         default:
             return state
     }
@@ -18,9 +24,21 @@ export const useFirestore = (collection)=>{
     const [response,dispatch] =useReducer(firestoreReducer,initialState)
     //getting a refernce to collection
     const ref = projectFirestore.collection(collection)
+    //dispatching action only allowed
+    const dispatchWhenCancelledIsFalse =(action)=>{
+        if(!cancelled){
+            dispatch(action)
+        }
+    }
     //adding document
-    const addDocument = (doc)=>{
-        
+    const addDocument = async(doc)=>{
+        dispatch({type:'IS_PENDING'})
+        try{
+            const addedDocument = await ref.add(doc)
+            dispatchWhenCancelledIsFalse({type:'ADDED_DOCUMENT',payload:addedDocument})
+        }catch(err){
+            dispatchWhenCancelledIsFalse({type:'ERROR',payload:err.message})
+        }
     }
     //deleting document
     const delDocument =(id)=>{
