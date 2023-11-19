aa51bfba781dd3e700ab14f5f737eac5891edb95
Ritabrata Pal <rito3668@gmail.com>
2023-11-19 11:04:20 +0530

    firestore hook modificatrion

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { useFirestore } from '../../hooks/useFirestore'
 export default function TransactionForm() {
     const [name,setName] = useState('')
     const [amount,setAmount] = useState('')
