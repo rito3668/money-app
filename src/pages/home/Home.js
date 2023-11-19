e2e7f484ff51428bb0077e9fb084c1ed07fcf8df
Ritabrata Pal <rito3668@gmail.com>
2023-11-19 11:47:08 +0530

    user id guys

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import styles from './Home.module.css'
 import TransactionForm from './TransactionForm'
+import {useAuthContext} from '../../hooks/useAuthContext'
 export default function Home() {
+  const {user} = useAuthContext()
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         Transaction List
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm/>
+        <TransactionForm uid={user.uid}/>
       </div>
     </div>
   )
--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import { useFirestore } from '../../hooks/useFirestore'
-export default function TransactionForm() {
+export default function TransactionForm({uid}) {
     const [name,setName] = useState('')
     const [amount,setAmount] = useState('')
+    const {addDocument,response} = useFirestore("transactions")
     const handleSubmit =(e)=>{
         e.preventDefault()
-        console.log({name,amount})
+        addDocument({name,amount,uid})
     }
     return (
     <>
