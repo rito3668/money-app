5ff8bb2b4435d5af1f11a7ef265f97ab26c3abbe
Ritabrata Pal <rito3668@gmail.com>
2023-11-29 14:29:16 +0530

    descending order

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,15 +1,18 @@
 import { projectFirestore } from "../firebase/config"
 import { useState,useEffect,useRef } from "react"
-export const useCollection = (collection,_query) =>{
+export const useCollection = (collection,_query,_orderBy) =>{
     const [documents,setDocuments] = useState(null)
     const [error,setError] = useState(null)
     const query = useRef(_query).current 
-
+    const orderBy = useRef(_orderBy).current
 
     useEffect(()=>{
         let ref = projectFirestore.collection(collection)
         if(query){
             ref = ref.where(...query)
+        }
+        if(orderBy){
+            ref = ref.orderBy(..._orderBy)
         }
         const unsub = ref.onSnapshot((snapshot)=>{
             let results =[]
