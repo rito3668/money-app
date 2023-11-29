45ca837d32cf51ebd524e6bef8d905317f9728fd
Ritabrata Pal <rito3668@gmail.com>
2023-11-29 14:12:25 +0530

    changing the useCollection

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,7 @@ import { useCollection } from '../../hooks/useCollection'
 import Transactionlist from '../../components/Transactionlist'
 export default function Home() {
   const {user} = useAuthContext()
-  const {documents,error} = useCollection("transactions")
+  const {documents,error} = useCollection("transactions",["uid","==",user.uid])
   return (
     <div className={styles.container}>
       <div className={styles.content}>
