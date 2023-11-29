a7058b1e8fe5503a656227951abad723819b7739
Ritabrata Pal <rito3668@gmail.com>
2023-11-29 14:47:16 +0530

    deleting docs

--- src/components/Transactionlist.js
+++ src/components/Transactionlist.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from '../pages/home/Home.module.css'
+import { useFirestore } from '../hooks/useFirestore'
 export default function Transactionlist({transactions}) {
+    const {delDocument} = useFirestore("transactions")
   return (
     <ul className={styles.transactions}>
         {transactions.map((transaction)=>(
             <li key={transaction.id}>
                 <p className={styles.name}>{transaction.name}</p>
                 <p className={styles.amount}>${transaction.amount}</p>
+                <button onClick={()=>delDocument(id)}>x</button>
             </li>
         ))}
     </ul>
